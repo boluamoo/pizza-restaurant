c4ae55196d3cab1848a23424d83da148a93a6801
boluamoo <boluamoo58@gmail.com>
2024-02-27 19:48:22 +0100

    added about and contact page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import MultiplePizzas from '../assets/multiplePizzas.jpeg'
+import '../styles/About.css'
+
 function About() {
   return (
     <div className="about">
@@ -7,7 +9,10 @@ function About() {
         className="aboutTop"
         style={{ backgroundImage: `url(${MultiplePizzas})` }}
       ></div>
-      <div className="aboutBottom"></div>
+      <div className="aboutBottom">
+        <h1>ABOUT US</h1>
+        <p>    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro commodi temporibus, nisi et voluptates, fuga ipsam assumenda odit, magnam quis expedita animi culpa excepturi ex eos fugit facilis incidunt sint provident nesciunt distinctio similique enim repellat nihil! Obcaecati minima tempore, molestias iste ducimus aut in esse voluptatibus asperiores repellat rem.    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro commodi temporibus, nisi et voluptates, fuga ipsam assumenda odit, magnam quis expedita animi culpa excepturi ex eos fugit facilis incidunt sint provident nesciunt distinctio similique enim repellat nihil! Obcaecati minima tempore, molestias iste ducimus aut in esse voluptatibus asperiores repellat rem.    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro commodi temporibus, nisi et voluptates, fuga ipsam assumenda odit, magnam quis expedita animi culpa excepturi ex eos fugit facilis incidunt sint provident nesciunt distinctio similique enim repellat nihil! Obcaecati minima tempore, molestias iste ducimus aut in esse voluptatibus asperiores repellat rem.</p>
+      </div>
     </div>
   )
 }
